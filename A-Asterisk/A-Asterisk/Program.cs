ee9c2e3f265c3202b098afcd358954591ecf24aa
Brian Smith <briansmith@rocketcentral.com>
2024-04-29 16:58:39 -0400

    Re-factored N-Choose-K to new app

---
+++ A-Asterisk/A-Asterisk/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AAsterisk
+{
+    class Example
+    {
+        class Location
+        {
+            public int x;
+            public int y;
+            public int Score1;
+            public int Score2;
+            public int Score3;
+            public Location Parent;
+            
+
+        }
+
+        static void Main(string[] args)
+        {
+            string[] map = new string[]
+            {
+                "+----------------+",
+                "|A               |",
+                "|XXXXXX          |",
+                "|            XXXX|",
+                "|                |",
+                "|XXXXXX     XXXXX|",
+                "|               B|",
+                "+----------------+"
+            };
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            foreach( var line in map )
+            {
+                Console.WriteLine(line);
+            }
+
+            Location current = null;
+            Location start = new Location { x = 1, y = 1 };
+            Location target = new Location { x = 16, y = 6 };
+            List<Location> openList = new List<Location>();
+            List<Location> closedList = new List<Location>();
+        }
+
+    }
+        
+}
+
---
+++ N Choose K Algorithm/N Choose K Algorithm/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace NChooseK
+{
+
+    public class Program
+    {
+        static int numberOfPossibleCombinations()
+        {
+            if (  k == 0 || k == n )
+            {
+                return 1;
+            }
+            else
+            {
+                return numberOfPossibleCombinations( n - 1, k ) + numberOfPossibleCombinations( n - 1, k - 1 );
+            }
+        }
+    }
+    
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+        System.Diagnostics.Debug.WriteLine(numberOfPossibleCombinations(3, 2));
+    }
+
+}
+
