b6b30b7ee4ab6cdc8d12cc19b391d3236faf8437
Brian Smith <briansmith@rocketcentral.com>
2024-05-02 11:02:50 -0400

    Added project for Dijkstra's Algorithm

---
+++ Dijkstras Algorithm /Dijkstras Algorithm /Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DijkstrasAlgorithm
+{
+    class Example
+    {
+        private static int MinimumDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
+        {
+            
+            int min = int.MaxValue;
+            int minIndex = 0;
+
+            for( int i = 0; i < verticesCount; i++ )
+            {
+                if( !shortestPathTreeSet[i] && distance[i] <= min )
+                {
+                    minIndex = i;
+                    min = distance[i];
+                }
+
+            }
+            
+        }
+        
+        return minIndex;
+
+    }
+    
+}
+
+// See https://aka.ms/new-console-template for more information
+
