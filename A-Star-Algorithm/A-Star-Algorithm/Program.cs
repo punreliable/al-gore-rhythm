ab505b1396c07bfb86a1a0bcd117e2ff262efa07
Brian Smith <briansmith@rocketcentral.com>
2024-05-02 10:38:59 -0400

    Lee's Algorithm written

--- A-Star-Algorithm/A-Star-Algorithm/Program.cs
+++ A-Star-Algorithm/A-Star-Algorithm/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace AStarAlgorithm
 {
-    class Example
+    class AStar
     {
         class Location
         {
@@ -14,125 +14,132 @@ namespace AStarAlgorithm
             public int Score2;
             public int Score3;
             public Location Parent;
-
-
         }
 
-        static void Main( string[] args )
-        {
-            string[] map = new string[]
+        static void Main(string[] args)
             {
-                "+----------------+",
-                "|A               |",
-                "|XXXXXX          |",
-                "|            XXXX|",
-                "|                |",
-                "|XXXXXX     XXXXX|",
-                "|               B|",
-                "+----------------+"
-            };
-
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-
-            foreach (var line in map)
-            {
-                Console.WriteLine(line);
-            }
-
-            Location current = null;
-            Location start = new Location { X = 1, Y = 1 };
-            Location target = new Location { X = 16, Y = 6 };
-            List<Location> openList = new List<Location>();
-            List<Location> closedList = new List<Location>();
-            int spot = 0;
-            
-            openList.Add(start);
-
-            while ( openList.Count > 0 )
-            {
-                int min = openList.Min(l => l.Score1);
-                current = openList.First(l => l.Score1 == min );
-                closedList.Add(current);
-                Console.SetCursorPosition(current.X,current.Y);
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write('.');
-                Console.SetCursorPosition(current.X,current.Y);
-                
-                System.Threading.Thread.Sleep(1000);
-                
-                openList.Remove(current);
-                if ( closedList.FirstOrDefault( l => l.X == target.X &&
-                                                l.Y == target.Y) != null )
+                string[] map = new string[]
                 {
-                    break;
+                    "+----------------+",
+                    "|A               |",
+                    "|XXXXXX          |",
+                    "|            XXXX|",
+                    "|                |",
+                    "|XXXXXX     XXXXX|",
+                    "|               B|",
+                    "+----------------+"
+                };
+
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+                foreach (var line in map)
+                {
+                    Console.WriteLine(line);
                 }
 
-                List<Location> adjacentSquares = GetMovableAdjacentSpots(current.X, current.Y, map);
-                
-                spot++;
+                Location current = null;
+                Location start = new Location { X = 1, Y = 1 };
+                Location target = new Location { X = 16, Y = 6 };
+                List<Location> openList = new List<Location>();
+                List<Location> closedList = new List<Location>();
+                int spot = 0;
 
-                foreach ( Location adjacentSquare in adjacentSquares )
+                openList.Add(start);
+
+                while (openList.Count > 0)
                 {
-                    if ( closedList.FirstOrDefault( l => l.X == adjacentSquare.X &&
-                                                         l.Y == adjacentSquare.Y) != null )
+                    int min = openList.Min( l => l.Score1 );
+                    current = openList.First( l => l.Score1 == min );
+                    closedList.Add(current);
+                    Console.SetCursorPosition( current.X, current.Y );
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write('.');
+                    Console.SetCursorPosition( current.X, current.Y );
+
+                    System.Threading.Thread.Sleep(1000);
+
+                    openList.Remove( current );
+                    if (closedList.FirstOrDefault( l => l.X == target.X &&
+                                                       l.Y == target.Y ) != null )
                     {
-                        continue;
+                        break;
                     }
-                    
-                    if ( openList.FirstOrDefault( l =>
-                            l.X == adjacentSquare.X && 
-                            l.Y == adjacentSquare.Y ) == null ) 
+
+                    List<Location> adjacentSquares = GetMovableAdjacentSpots( current.X, current.Y, map );
+
+                    spot++;
+
+                    foreach ( Location adjacentSquare in adjacentSquares )
                     {
-                        adjacentSquare.Score2 = spot;
-                        adjacentSquare.Score3 = ComputeSpotHeuristic( adjacentSquare.X, adjacentSquare.Y, target.X, target.Y );
-                        adjacentSquare.Score1 = adjacentSquare.Score2 + adjacentSquare.Score3;
-                        adjacentSquare.Parent = current;
-                        openList.Insert(0, adjacentSquare);
-                        
-                    } else {
-                        if ( spot + adjacentSquare.Score3 < adjacentSquare.Score1 ) {
+                        if ( closedList.FirstOrDefault( l => l.X == adjacentSquare.X && l.Y == adjacentSquare.Y ) != null )
+                        {
+                            continue;
+                        }
+
+                        if ( openList.FirstOrDefault( l =>
+                                l.X == adjacentSquare.X &&
+                                l.Y == adjacentSquare.Y ) == null )
+                        {
                             adjacentSquare.Score2 = spot;
+                            adjacentSquare.Score3 =
+                                ComputeSpotHeuristic( adjacentSquare.X, adjacentSquare.Y, target.X, target.Y );
                             adjacentSquare.Score1 = adjacentSquare.Score2 + adjacentSquare.Score3;
                             adjacentSquare.Parent = current;
+                            openList.Insert( 0, adjacentSquare );
+
                         }
+                        else
+                        {
+                            if ( spot + adjacentSquare.Score3 < adjacentSquare.Score1 )
+                            {
+                                
+                                adjacentSquare.Score2 = spot;
+                                adjacentSquare.Score1 = adjacentSquare.Score2 + adjacentSquare.Score3;
+                                adjacentSquare.Parent = current;
+                                
+                            }
+                            
+                        }
+                        
                     }
-                }
 
-                while ( current != null )
-                {
-                    Console.SetCursorPosition(current.X, current.Y);
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write('o');
-                    Console.SetCursorPosition(current.X, current.Y);
-                    current = current.Parent;
-                    
-                    System.Threading.Thread.Sleep(1000);
-                    
-                }
-
-                Console.ReadLine();
-                
-                static List<Location> GetMovableAdjacentSpots( int x, int y, string[] map )
-                {
-                    List<Location> proposedLocations = new List<Location>()
+                    while ( current != null )
                     {
-                        new Location { X = x, Y = y - 1 },
-                        new Location { X = x, Y = y + 1 },
-                        new Location { X = x - 1, Y = y },
-                        new Location { X = x + 1, Y = y }
-                    };
+                        Console.SetCursorPosition( current.X, current.Y );
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write( 'o' );
+                        Console.SetCursorPosition( current.X, current.Y );
+                        current = current.Parent;
 
-                    return proposedLocations.Where( l => map[l.Y][l.X] == ' ' || 
-                                                                map[l.Y][l.X] == 'B' ).ToList();
+                        System.Threading.Thread.Sleep( 1000 );
+
+                    }
+
+                    Console.ReadLine();
+
+                    static List<Location> GetMovableAdjacentSpots( int x, int y, string[] map )
+                    {
+                        List<Location> proposedLocations = new List<Location>()
+                        {
+                            new Location { X = x, Y = y - 1 },
+                            new Location { X = x, Y = y + 1 },
+                            new Location { X = x - 1, Y = y },
+                            new Location { X = x + 1, Y = y }
+                        };
+
+                        return proposedLocations.Where( l => map[l.Y][l.X] == ' ' || map[l.Y][l.X] == 'B' ).ToList();
+
+                    }
+
+                    static int ComputeSpotHeuristic( int x, int y, int targetX, int targetY )
+                    {
+                        return Math.Abs(targetX - x) + Math.Abs(targetY - y);
+                    }
+
                 }
 
-                static int ComputeSpotHeuristic( int x, int y, int targetX, int targetY )
-                {
-                    return Math.Abs(targetX - x) + Math.Abs(targetY - y);
-                }
             }
+
         }
 
     }
-
-}
---
+++ Lee Algorithm/Lee Algorithm/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace LeesAlgorithm
+{
+
+    class Example
+    {
+        public class Node
+        {
+
+            public int x;
+            public int y;
+            public int distance;
+            
+            public Node( int x, int y, int distance )
+            {
+                
+                this.x = x;
+                this.y = y;
+                this.distance = distance;
+                
+            }
+
+        }
+
+        public static int[] row = { -1, 0, 0, 1 };
+        public static int[] column = { 0, -1, 1, 0 };
+
+        private static bool IsValid( int[][] matrix, bool[][] visited, int row, int col )
+        {
+            return ( row >= 0 ) && ( row < 10 ) && ( col >= 0 ) && ( col < 10 ) && matrix[row][col] == 1 && !visited[row][col];
+        }
+        
+        private static void LeeAlgorithm( int[][] matrix, int i, int j, int x, int y )
+        {
+            bool[][] visited = new bool[10][];
+
+            visited[0] = new bool[10];
+            visited[1] = new bool[10];
+            visited[2] = new bool[10];
+            visited[3] = new bool[10];
+            visited[4] = new bool[10];
+            visited[5] = new bool[10];
+            visited[6] = new bool[10];
+            visited[7] = new bool[10];
+            visited[8] = new bool[10];
+            visited[9] = new bool[10];
+
+            Queue<Node> queue = new Queue<Node>();
+            visited[i][j] = true;
+            queue.Enqueue( new Node( i, j, 0 ) );
+            int minimumDistance = int.MaxValue;
+
+            while ( queue.Count > 0 )
+            {
+                Node node = queue.Dequeue();
+
+                i = node.x;
+                j = node.y;
+                int dist = node.distance;
+
+                if ( i == x && j == y )
+                {
+                    minimumDistance = dist;
+                    break;
+                }
+
+                for( int k = 0; k < 4; k++ ) {
+
+                    if( IsValid( matrix, visited, i + row[k], j + column[k] ) )
+                    {
+                        visited[i + row[k]][j + column[k]] = true;
+                        queue.Enqueue(new Node( i + row[k], j + column[k], dist + 1 ));
+                    }
+
+                }
+            }
+
+            if (minimumDistance != int.MaxValue)
+            {
+                Debug.WriteLine( "The shortest path from source to destination has length: " + minimumDistance );
+            }
+            else
+            {
+                Debug.WriteLine( "Destination can't be reached from the given source" );
+            }
+        }
+
+        public static void Main( String[] args )
+        {
+            
+            int[][] mat = new int[10][];
+
+            mat[0] = new int[10] { 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 };
+            mat[1] = new int[10] { 0, 1, 1, 1, 1, 1, 0, 1, 0, 1 };
+            mat[2] = new int[10] { 0, 0, 1, 0, 1, 1, 1, 0, 0, 1 };
+            
+            mat[3] = new int[10] { 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 };
+            mat[4] = new int[10] { 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 };
+            mat[5] = new int[10] { 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 };
+            mat[6] = new int[10] { 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 };
+            mat[7] = new int[10] { 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 };
+            mat[8] = new int[10] { 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 };
+            mat[9] = new int[10] { 0, 0, 1, 0, 0, 1, 1, 0, 0, 1 };
+
+            LeeAlgorithm(mat, 0, 0, 6, 4 );
+        }
+    }
+}
