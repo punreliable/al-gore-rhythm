08d5aa1823a890d66a67fd02f6e99e69822f13c6
Brian Smith <briansmith@rocketcentral.com>
2024-05-02 16:47:03 -0400

    Added FloydWarshall Algorithm, but no main, does not run

---
+++ Floyd Warshall Algorithm/Floyd Warshall Algorithm/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace FloydWarshall
+{
+    class Example
+    {
+        public static void FloydWarshall( int[,] graph, int verticesCount )
+        {
+            int[,] distance = new int[verticesCount, verticesCount];
+
+            for (int k = 0; k < verticesCount; k++)
+            {
+
+                for (int i = 0; i < verticesCount; i++) 
+                {
+                    
+                    for (int j = 0; j < verticesCount; j++)
+                    {
+                        
+                        if (distance[i, k] + distance[k, j] < distance[i, j])
+                        {
+
+                            distance[i, j] = distance[i, k] + distance[k, j];
+
+                        }
+                        
+                    }
+                    
+
+                }
+
+            }
+            
+        }
+        
+    }
+
+}
+
+
