e626f96718023eae585ee8a7759e0a3a5d607d28
Brian Smith <briansmith@rocketcentral.com>
2024-05-03 09:33:24 -0400

    Added Floyd's Tortoise & Hare Algorithm, again, no Main() method to run

---
+++ FloydsTortoiseAndHareAlgorithm/FloydsTortoiseAndHareAlgorithm/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace FloydsTortoiseAndHare
+{
+
+    class TortoiseAndHare
+    {
+        
+        public static bool FloydCycleDetection<T>(LinkedList<T> list)
+        {
+            
+            if (list.Count <= 2)
+            {
+                
+                return false;
+                
+            }
+
+            LinkedListNode<T> tortoise = list.First.Next;
+            LinkedListNode<T> hare = list.First.Next.Next;
+
+            while ( tortoise != null && hare != null )
+            {
+        
+                if ( tortoise == hare )
+                {
+                    
+                    return true;
+                    
+                }
+
+                if ( hare.Next != null )
+                {
+                    hare = hare.Next.Next;
+                    
+                }
+
+                tortoise = tortoise.Next;
+
+            }
+
+            return false;
+
+        }
+        
+    }
+
+}
